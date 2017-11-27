785b3ce724e7569e571c510c5d2d74477e49e568
juedizor <juedizor@gmail.com>
2017-11-27 18:15:16 -0500

    se actualiza el codigo de subir imagenes

--- app/controller/uploadImagesCtrl.js
+++ app/controller/uploadImagesCtrl.js
@@ -5,7 +5,8 @@
 */
 
 var uploadImage = require("../model/rutaImagenes"),
-    fs = require('fs');
+    fs = require('fs'),
+    path = require('path');
 
 var modelRutaImagen = uploadImage.rutaImagenesModel;
 var ruta = "/home/julio/imagenes_lyra/";
@@ -14,46 +15,57 @@ var ruta = "/home/julio/imagenes_lyra/";
 function guardarImagen(req, res) {
   modelRutaImagen.findOne({idDoc:req.body.id_doc}, function(err, dataRutaImagen){
     if(err) return res.status(500).send(err);
-    console.log(dataRutaImagen)
     if(dataRutaImagen != null) {
-
-
-    }else{
-      if(req.files){
-        // debe copiar el file en la ruta
-        var file = ruta + req.files.myFile.originalFilename;
-        fs.mkdir(ruta, function(err){
-          if(err){
-              if(err.code === 'EEXIST'){
-                return openCopyFile(req, res, file);
-              }else{
-                deleteFile(req.files.myFile.path);
-                return res.status(500).send("Error creando directorio "+ruta);
-              }
-          }else{
-            return openCopyFile(req, res, file);
-          }
-        });
-      }else{
-        return res.status(500).send('Falta la imagen del documento');
+      if(!isEmpty(req.files)) {
+        return realizarProcesoCargueImagen(req, res, true);
       }
-
+    }else{
+      return realizarProcesoCargueImagen(req, res, false);
     }
   })
-
 }
 
-function openCopyFile(req, res, file){
+function realizarProcesoCargueImagen(req, res, exits) {
+  if(!isEmpty(req.files)) {
+    // debe copiar el file en la ruta
+    var pathExt = path.extname(req.files.myFile.name)
+    var file = ruta + req.body.id_doc + pathExt;
+    fs.mkdir(ruta, function(err){
+      if(err){
+          if(err.code === 'EEXIST'){
+            return openCopyFile(req, res, file, exits);
+          }else{
+            deleteFile(req.files.myFile.path);
+            return res.status(500).send("Error creando directorio "+ruta);
+          }
+      }else {
+        return openCopyFile(req, res, file, exits);
+      }
+    });
+  }else{
+    return res.status(500).send('Falta la imagen del documento');
+  }
+}
+
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
+
+function openCopyFile(req, res, file, exits){
   fs.open(file, 'w', function(err, data){
     if (err) {
       return res.status(500).send('Error abriendo archivo '+file);
-    }else{
-      return copyFile(req, res, file);
+    }else {
+      return copyFile(req, res, file, exits);
     }
   })
 }
 
-function copyFile(req, res, file){
+function copyFile(req, res, file, exits){
   fs.copyFile(req.files.myFile.path,
     file,
     function(err) {
@@ -62,26 +74,43 @@ function copyFile(req, res, file){
         return res.status(500).send(err);
       }
 
-      // guarda la imagen con la ruta
-      var rutaImagenes = new modelRutaImagen({
-        idDoc: req.body.id_doc,
-        ruta: "/home/julio/imagenes_lyra/"+req.files.myFile.originalFilename
-      });
+      if(exits){
+          var query = { idDoc: req.body.id_doc };
+          modelRutaImagen.findOneAndUpdate (query,
+            {ruta: ruta + req.files.myFile.originalFilename},
+            {new: true},
+            function(err, modelRutaImagen){
+                deleteFile(req.files.myFile.path);
+                if(err){
+                  return res.status(500).send(err);
+                }
+                return res.status(200).jsonp(modelRutaImagen);
 
-      rutaImagenes.save(function(err, rutaImagenes){
-        if(err) {
+          })
+      }else{
+        // guarda la imagen con la ruta
+        var rutaImagenes = new modelRutaImagen({
+          idDoc: req.body.id_doc,
+          ruta: ruta + req.files.myFile.originalFilename
+        });
+
+        rutaImagenes.save(function(err, rutaImagenes){
           deleteFile(req.files.myFile.path);
-          return res.status(500).send ("Error al realizar el cargue de la imagen");
-        }
-        deleteFile(req.files.myFile.path);
-        return res.status(200).jsonp(rutaImagenes);
-      });
+          if(err) {
+            return res.status(500).send ("Error al realizar el cargue de la imagen");
+          }
+          return res.status(200).jsonp(rutaImagenes);
+        });
+      }
+
+
     });
 }
 
 function deleteFile(path){
   fs.unlinkSync(path);
 }
+
 function consutarImagenIdDoc(req, res){
   modelRutaImagen.find({idDoc: req.params.id_doc}, function(err, modelRutaImagen){
     if(err) return res.status(500).send("Error con busqueda de registros para el idDoc "+req.params.id_doc);
