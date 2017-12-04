8358795e9f42e593ce8cc4a97e91b88ee2c15903
juedizor <juedizor@gmail.com>
2017-12-03 20:27:08 -0500

    list files dir and subdirs

--- app/middlewares/particion_disponible.js
+++ app/middlewares/particion_disponible.js
@@ -1,5 +1,7 @@
 var particiones = require('../model/particiones'),
-  diskspace = require('diskspace');
+  diskspace = require('diskspace'),
+  fs = require('fs'),
+  dir = require('path-reader');
 
 var particionesModel = particiones.particionesModel;
 var KB = 1024;
@@ -8,20 +10,19 @@ function getDiscoDisponible(req, res) {
   particionesModel.find({
     disponible: true
   }, function(err, data) {
-
     getDiskFree(data)
 
 
-/*
-    diskspace.check(data.ruta_particion, function(err, result) {
-      if (err) return res.status(500).send("Error con el disco " + data.ruta_particion);
-      console.log(result)
-      var total = result.total / KB;
-      var used = result.used / KB;
-      var free = result.free / KB;
-      console.log("total: " + total + "\n" + "used:  " + used + "\n" + "free: " + free);
-    });
-    */
+    /*
+        diskspace.check(data.ruta_particion, function(err, result) {
+          if (err) return res.status(500).send("Error con el disco " + data.ruta_particion);
+          console.log(result)
+          var total = result.total / KB;
+          var used = result.used / KB;
+          var free = result.free / KB;
+          console.log("total: " + total + "\n" + "used:  " + used + "\n" + "free: " + free);
+        });
+        */
 
   })
 
@@ -29,10 +30,17 @@ function getDiscoDisponible(req, res) {
 
 
 function getDiskFree(data) {
-  for (var i = 0; i < data.length; i++){
+  for (var i = 0; i < data.length; i++) {
     var rutaParticion = data[i].ruta_particion;
-    console.log(rutaParticion)
+    var files = dir.files(rutaParticion, {
+      sync: true
+    });
+    
   }
+
+}
+
+function getCantFiles() {
 
 }
 
